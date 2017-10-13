30e517606b9801d268eb4c788833f2c652a78063
RubyTear <30708722+RubyTear@users.noreply.github.com>
2017-10-13 18:15:41 +0900

    Create Block.java

---
+++ java/application/Block.java
@@ -0,0 +1,5 @@
+package com.ruby.crush;
+public enum Block { YELLOW(1), PINK(2), BLUE(3), ALL(4); private int num;
+ private Block(int num) {  this.num = num; }
+ public int getValue() {  return this.num; }
+ public static Block getType(final int id) {  Block[] BlockTypes = Block.values();  for (Block type : BlockTypes) {   if (type.getValue() == id) {    return type;   }  }  return null; }}
