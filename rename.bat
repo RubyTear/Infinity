0c6fea97b7c8274080dc7a1077551cc1ec87c045
RubyTear <30708722+RubyTear@users.noreply.github.com>
2018-04-04 16:53:20 +0900

    Create rename.bat

---
+++ rename.bat
@@ -0,0 +1,29 @@
+@ECHO OFF
+SETLOCAL ENABLEDELAYEDEXPANSION
+
+SET path=%~dp0
+SET filetype=txt
+
+ECHO [FILE_TYPE]:%filetype%
+
+FOR /R %path% %%a IN (*.%filetype%) DO (
+SET file=%%a
+SET filename=%%~na
+FOR /F “tolen=1,* delims==“ %%x IN (%path%filename.properties) DO (
+SET targetfilename=%%x
+SET newfilename=%%y
+IF !filename!==!targetfilename! (
+CALL :RENAME_FILE !file! !newfilename! %filetype%
+)
+)
+)
+PAUSE
+GOTO :EOF
+
+:RENAME_FILE
+IF EXIST %1 (
+SET oldFile=%1
+SET newFile=%2.%3
+ECHO [SUCCEED] [FROM]!oldFile! [TO]!newFile!
+REN !oldFile! !newFile!
+)
